7f1ce84233a99322848875a8b2aae108a6aca9ff
mayaaugustine <Oct2019#>
2019-10-12 19:46:13 -0400

    Assignment2

--- assign1.py
+++ assign1.py
@@ -1,10 +1,10 @@
 ##A sequence consists of integer numbers and ends with the number 0.
-###Determine how many elements of this sequence are greater than their neighbors above.
+##Determine how many elements of this sequence are greater than their neighbors above.
 a = [10,30,50,100,60,190,5650,55650,600]
 b = len(a)
 count=0
 for i in range(0,b-1) :
      if a[i+1] > a[i]:
         print(a[i+1])
-        count+=1
+
 
