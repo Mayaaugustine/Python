09fb4f286051550499b11dc81a918925907ec295
mayaaugustine <Oct2019#>
2019-10-15 22:04:17 -0400

    Assignment4

---
+++ assign5.py
@@ -0,0 +1,4 @@
+#Given a list of numbers, swap adjacent items in pairs (A[0] with A[1], A[2] with A[3], etc.).
+#Print the resulting list. If a list has an odd number of elements, leave the last element in place.
+
+a = [10,30,50,100,60,190,5650,5560,6000]
