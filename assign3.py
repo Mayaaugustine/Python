4b42efec09a8e12b5af224ebac9c943a9cb2392b
mayaaugustine <Oct2019#>
2019-10-16 22:47:05 -0400

    Assignment5

--- assign3.py
+++ assign3.py
@@ -1,9 +1,9 @@
 #Given a list of numbers, determine and print the quantity of elements that are greater than both of their neighbors.
 #The first and the last items of the list shouldn't be considered because they don't have two neighbors.
-a = [100,30,50,100,60,190,5650,55650,600]
+a = [1000,300,50,100,60,19000,5650,55650,60000]
 b = len(a)
 count=0
-for i in range(0,b-1) :
+for i in range(0,b-2) :
     if a[i+1] > a[i]:
          if a[i + 1] > a[i+2]:
             print(a[i+1])
--- assign5.py
+++ assign5.py
@@ -2,3 +2,18 @@
 #Print the resulting list. If a list has an odd number of elements, leave the last element in place.
 
 a = [10,30,50,100,60,190,5650,5560,6000]
+x= len(a)
+b=[]
+
+z=x%2
+print(z)
+for i in range(0,x-1,2) :
+    b.append(a[i+1])
+    b.append(a[i])
+    print(i)
+    if x%2 > 0:
+        print(i,x)
+        if i>x:
+            b.append(a[x-1])
+
+print(b)
